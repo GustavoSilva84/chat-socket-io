9109358ea62c5178bad2e4843c62dd411a65ac3c
Gustavo-twi <72147773+Gustavo-Silva-01@users.noreply.github.com>
2021-04-03 23:25:07 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
 
-const http = require('http')
-const serverHttp = http.createServer(app);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const socketIo = require('socket.io');
-const io = socketIo(serverHttp);
-
-app.use(express.static(`${__dirname}/public`));
-let mensagens = [];
+const chalk = require('chalk');
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/html/index.html`);
 });
+
+let mensagens = [];
 
 io.on('connection', (socket) => {
 
@@ -20,10 +18,9 @@ io.on('connection', (socket) => {
 
     socket.on('enviar mensagem', (dados) => {
 
-        if(dados.msg.length <= 2000) {
-            mensagens.push(dados);
-            io.emit('mostrar mensagem', dados)
-        }
+        mensagens.push(dados);
+        console.log(mensagens);
+        io.emit('mostrar mensagem', dados)
 
     });
 
@@ -31,9 +28,13 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('mostrar usuario dijitando', dados)
     })
 
+    // socket.on('usuario nao dijitando', () => {
+    //     socket.broadcast.emit('mostrar nao usuario dijitando')
+    // })
+
 })
 
-serverHttp.listen(process.env.PORT || 8181, erro => {
+http.listen(8181, erro => {
 
     if(erro) {
         return console.log(`Erro: ${erro}`);
@@ -42,6 +43,3 @@ serverHttp.listen(process.env.PORT || 8181, erro => {
     return console.log(`Servidor iniciado`);
 
 });
-
-
-
